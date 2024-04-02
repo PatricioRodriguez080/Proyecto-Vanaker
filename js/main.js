526fdf77412520269266a800ec5b1c5b9587656d
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-04-02 12:05:30 -0300

    Agregado de links swipper js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.card-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    pagination: {
+        el: '.card-slider__pagination',
+        clickable: true,
+    }
+});
+
+
