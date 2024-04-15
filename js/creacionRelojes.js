c374c0f8f7c1bdd6e1a68a584ec5e5cf14d0f532
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-04-15 12:22:50 -0300

    Agregado de dos marcas con sus respectivos arrays y modificacion de funcion crearTarjetasRelojes

--- js/creacionRelojes.js
+++ js/creacionRelojes.js
@@ -1,4 +1,4 @@
-// Traigo a JS el contenedor //
+/*/ Traigo a JS el contenedor //
 const contenedorTarjetas = document.getElementById("container-relojes")
 
 // Ejecuto la funcion para crear las cards de relojes //
@@ -11,6 +11,33 @@ function crearTarjetasRelojes(arrayRelojes) {
             <div class="col-sm-12 col-md-6 col-lg-4 col-relojes">
                 <div class="card" style="width: 18rem;">
                     <img src="../media/relojes/${reloj.id}.jpg" class="card-img-top" alt="reloj ${reloj.marca}">
+                    <div class="card-body">
+                        <h5 class="card-title">${reloj.nombre}</h5>
+                        <h6 class="marca">${reloj.marca}</h6>
+                        <div class="container-precio">
+                            <h5 class="card-title">$${reloj.precio}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+}*/
+
+const contenedorTarjetasCasio = document.getElementById("container-casio")
+const contenedorTarjetasMistral = document.getElementById("container-mistral")
+const contenedorTarjetasPrune = document.getElementById("container-prune")
+
+crearTarjetasRelojes(arrayCasio, contenedorTarjetasCasio)
+crearTarjetasRelojes(arrayMistral, contenedorTarjetasMistral)
+crearTarjetasRelojes(arrayPrune, contenedorTarjetasPrune)
+
+function crearTarjetasRelojes(arrayRelojes, contenedor) {
+    arrayRelojes.forEach((reloj) => {
+        contenedor.innerHTML += `
+            <div class="col-sm-12 col-md-6 col-lg-4 col-relojes">
+                <div class="card" style="width: 18rem;">
+                    <img src="../media/relojes/${reloj.marca}/${reloj.id}.jpg" class="card-img-top" alt="reloj ${reloj.marca}">
                     <div class="card-body">
                         <h5 class="card-title">${reloj.nombre}</h5>
                         <h6 class="marca">${reloj.marca}</h6>
--- js/relojes.js
+++ js/relojes.js
@@ -1,4 +1,4 @@
-const arrayRelojes = [
+const arrayCasio = [
     {
         id: "A-100WE-1A",
         nombre: "Reloj Vintage",
@@ -36,4 +36,58 @@ const arrayRelojes = [
         precio: 99.80
     }
 
+]
+
+const arrayMistral = [
+    {
+        id: "CHI-2026S-01",
+        nombre: "RELOJ MISTRAL",
+        marca: "mistral",
+        precio: 87.90 
+    },
+    {
+        id: "CHI-2026S-02",
+        nombre: "RELOJ MISTRAL",
+        marca: "mistral",
+        precio: 87.90 
+    },
+    {
+        id: "CHT-7263-08",
+        nombre: "RELOJ MISTRAL",
+        marca: "mistral",
+        precio: 78 
+    },
+    {
+        id: "CHT-7263-1A",
+        nombre: "RELOJ MISTRAL",
+        marca: "mistral",
+        precio: 75.50 
+    },
+]
+
+const arrayPrune = [
+    {
+        id: "PRG-0207-01",
+        nombre: "RELOJ PRUNE",
+        marca: "prune",
+        precio: 42.90
+    },
+    {
+        id: "PRG-1002-01",
+        nombre: "RELOJ PRUNE",
+        marca: "prune",
+        precio: 43.50 
+    },
+    {
+        id: "PRG-1090-01",
+        nombre: "RELOJ PRUNE",
+        marca: "prune",
+        precio: 43.50 
+    },
+    {
+        id: "PRG-247-01",
+        nombre: "RELOJ PRUNE",
+        marca: "prune",
+        precio: 48.90 
+    },
 ]
